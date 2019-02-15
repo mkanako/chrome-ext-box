46ac66b2734316c3aeacbf67b6f8feaccea468db
kanako <617273720@qq.com>
2019-02-15 16:55:03 +0800

    optimize options page

--- src/options.js
+++ src/options.js
@@ -19,56 +19,74 @@ Vue.use(Input)
 
 Vue.config.productionTip = false
 
-chrome.storage.sync.get(['list'], result => {
-  const list = result.list || []
+Promise.all([
+  new Promise((resolve, reject) => {
+    chrome.storage.sync.get(['list'], result => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError)
+      } else {
+        resolve(result)
+      }
+    })
+  }),
+  new Promise((resolve, reject) => {
+    chrome.management.getAll(result => {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError)
+      } else {
+        resolve(result)
+      }
+    })
+  })
+]).then(result => {
+  const list = result[0].list || []
   const listID = {}
   for (let i = 0; i < list.length; i++) {
     listID[list[i].id] = i
   }
-  chrome.management.getAll(result => {
-    new Vue({
-      created () {
-        document.title = this.$i18n('extName')
-      },
-      data: {
-        ExtList: result.filter(item => {
-          if (listID[item.id] !== undefined) {
-            item.isAdd = true
-            list[listID[item.id]].name = item.name
-            list[listID[item.id]].icons = item.icons
-          }
-          return (
-            item.type === 'extension' &&
-            item.id !== chrome.i18n.getMessage('@@extension_id')
-          )
-        }),
-        list,
-        searchKey: '',
-      },
-      computed: {
-        CurExtList () {
-          if (this.searchKey.trim() !== '') {
-            return this.ExtList.filter(item => {
-              return (
-                item.name.toLowerCase().indexOf(this.searchKey.toLowerCase()) >=
-                0
-              )
-            })
-          } else {
-            return this.ExtList
-          }
-        },
-      },
-      watch: {
-        list (val) {
-          chrome.storage.sync.set({
-            list: val.map(item => {
-              return { id: item.id }
-            }),
-          })
-        },
-      },
-      render: h => h(App),
-    }).$mount('#app')
+  const ExtList = result[1].filter(item => {
+    if (listID[item.id] !== undefined) {
+      item.isAdd = true
+      list[listID[item.id]].name = item.name
+      list[listID[item.id]].icons = item.icons
+    }
+    return (
+      item.type === 'extension' &&
+      item.id !== chrome.i18n.getMessage('@@extension_id')
+    )
   })
+  app.list = list
+  app.ExtList = ExtList
 })
+
+const app = new Vue({
+  created () {
+    document.title = this.$i18n('extName')
+  },
+  data: {
+    ExtList: [],
+    list: [],
+    searchKey: '',
+  },
+  computed: {
+    CurExtList () {
+      if (this.searchKey.trim() !== '') {
+        return this.ExtList.filter(item => {
+          return item.name.toLowerCase().indexOf(this.searchKey.toLowerCase()) >= 0
+        })
+      } else {
+        return this.ExtList
+      }
+    },
+  },
+  watch: {
+    list (val) {
+      chrome.storage.sync.set({
+        list: val.map(item => {
+          return { id: item.id }
+        }),
+      })
+    },
+  },
+  render: h => h(App),
+}).$mount('#app')
